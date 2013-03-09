2dc30b7ee126687aa48abd66c965808c07ff6c72
develar <develar@gmail.com>
2013-03-09 13:33:38 +0400

    version as generated

---
+++ v8/protocol-model/generated/org/jetbrains/v8/protocol/output/Version.java
@@ -0,0 +1,13 @@
+// Generated source
+package org.jetbrains.v8.protocol.output;
+
+/**
+ * Reports version of the running V8
+ */
+public final class Version extends org.jetbrains.v8.protocol.V8Request {
+
+  @Override
+  public String getMethodName() {
+    return "version";
+  }
+}
